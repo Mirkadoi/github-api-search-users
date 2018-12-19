557447928e37d9e2be15bdd0dfb102e8cf03146d
Mirkadoi <iskanderka125@gmail.com>
2018-12-19 18:15:00 +0300

    0.85

--- src/App.js
+++ src/App.js
@@ -5,40 +5,46 @@ import Users from './User';
 const API = 'https://api.github.com/search/users?q=';
 
 class App extends Component {
+  render() {  
+    return (
+      <div className="App">
+        <input type="text" placeholder='Введите ник' value={this.state.value} onChange={this.handleChange} maxLength="12"/> 
+        {this.state.value !== '' &&
+        <button onClick={this.getUser} >Поиск</button>
+        }
+        <pre onChange={this.greeting}>{this.greeting ()}</pre>
+      </div>
+    );
+  }
+
   state ={
+      value: '',
       items: [
-        
       ]
   }
 
   getUser = () => {
-    const nick= this.refs.nick.value;
+    const nick= this.state.value;
     fetch (API+nick)
     .then(response => response.json())
     .then (data =>{
       this.setState({
           items: data.items
       })
-      
     })
+    .catch(err => alert('Ошибка:' + err))
   }
-  Greeting () {
+  greeting = () => {
     const {listLogin} = this.props
-    if (this.state.items.length===30) {
+    if (this.state.items.length > 0) {
       return <Users listLogin={listLogin} items={this.state.items}/>;
-    }else{
-      return;
     }
-    
+    else{
+      return 'Введите ник и нажмите "Поиск"';
+    }
   }
-  render() {  
-    return (
-      <div className="App">
-        <input type="text" placeholder='Введите ник' ref="nick" /> 
-        <button onClick={this.getUser} >Узнать логины</button>
-        <pre>{this.Greeting ()}</pre>
-      </div>
-    );
+  handleChange =(event) => {
+    this.setState({value: event.target.value});
   }
 }
 
